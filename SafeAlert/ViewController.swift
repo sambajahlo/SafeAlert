bdec1f3d67645a1df72f67dbad4a864891d31a7c
Samba Diallo <shabashiki@gmail.com>
2019-01-15 18:33:34 -0800

    Added map and working on showing users location

--- SafeAlert/ViewController.swift
+++ SafeAlert/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Setting location to San Francisco
+        let sanFrancisco = CLLocation(latitude: 37.773972, longitude: -122.431297)
+        
+        //Get user location
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 5
+        locationManager.startUpdatingLocation()
+        goToLocation(location: locationManager.location ?? sanFrancisco)
+        
+        
     }
 
+    
+    func goToLocation(location:CLLocation){
+        let span = MKCoordinateSpan(latitudeDelta: 0.1,longitudeDelta: 0.1)
+        let reigon = MKCoordinateRegion(center: location.coordinate,span: span)
+        mapView.setRegion(reigon, animated: false)
+    }
 
 }
 
